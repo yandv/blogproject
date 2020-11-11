388d82be2f52db63b395fff114f7987490eb139d
yandv <allangamerbr252@gmail.com>
2020-11-11 19:11:10 -0300

    Remake the project

---
+++ src/component/default/header.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+
+    render() {
+        return (
+            <header className="App-header">
+                <p> Edit <code>src/App.tsx</code> and save to reload. </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >Learn React</a>
+            </header>
+        )
+    }
+
+}
---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from './component/default/footer';
+import Header from './component/default/header';
+
+export default function Router() {
+    return (
+      <div className="App">
+        <Header/>
+        <Footer/>
+      </div>
+    );
+  };
